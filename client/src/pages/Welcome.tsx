e5699f542561066866de205c91fc8c362678cfd8
unknown <Administrador@SEAI-DIC012.seai.am.gov.br>
2024-12-02 12:26:43 -0400

    perf: welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,11 +1,12 @@
 import Hours from "../components/Hours"
+import Button from "../components/Button"
 
 import notebook_img from "../assets/svg/notebook.svg"
 import buble from "../assets/svg/Vector (3).png"
 import bubble2 from "../assets/svg/bublle2.svg"
-import { Link } from "react-router-dom"
 
 function Welcome() {
+
   return (
     <div className="flex flex-row">
         <div className="w-3/5">
@@ -19,8 +20,8 @@ function Welcome() {
 
         <div className="right-side">
             <span className="text-8xl font-bold text-left inset-x-50px relative top-56 left-40">Welcome to forms</span>
-            <Link to="/login"><button className="btn absolute top-2/4 left-2/3">Login</button></Link>
-            <button className="btn absolute top-2/4 left-3/4">Sign Up</button>
+            <Button to="/login" label="Login"/>
+            <Button to="/signup" label="Sign Up"/>
         </div>
     </div>
   )
